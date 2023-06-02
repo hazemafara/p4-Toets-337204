fbd1edbd5af9855604bcbb2bf03c3b6897bde008
hazemafara <566508@edu.rocmn.nl>
2023-06-02 15:41:55 +0200

    s

--- BE-P4-Toets-336515-main/app/db/Mvc-2209c-P4.sql
+++ BE-P4-Toets-336515-main/app/db/Mvc-2209c-P4.sql
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 -- Tabelstructuur voor tabel `Examen`
 --
 
-DROP TABLE IF EXISTS `Examen`;
+DROP TABLE IF EXSTS `Examen`;
 CREATE TABLE IF NOT EXISTS `Examen` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `StudentId` int(11) NOT NULL,
